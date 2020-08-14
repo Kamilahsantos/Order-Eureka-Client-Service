b9ccab8f0373842373c9bd51e1bd08c1924c5ae8
kamilah santos <kahsantos89@gmail.com>
2020-08-13 22:19:03 -0300

    crud order

--- src/main/java/com/livecoding/dio/order/repository/OrderRepository.java
+++ src/main/java/com/livecoding/dio/order/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.livecoding.dio.order.repository;
 
-public class OrderRepository {
+import com.livecoding.dio.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository <Order,Long> {
 }
